c3a7d94ae9ee1928d0d3354df1f1b90e0fca49ae
VnuckDima <vnuchok311994@hotmail.com>
2026-09-21 23:38:55 +0300

    fixed: added url for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/landing-page/',
+  
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        catalogue: 'catalogue.html',
+        main: resolve(__dirname, 'index.html'),
+        catalogue: resolve(__dirname, 'catalogue.html'),
       },
     },
   },
